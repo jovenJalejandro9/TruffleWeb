7fb6864a4a8ceb2a92e3a46a9d80bf1f9d71e187
jovenJalejandro9 <alexmorenoj9.am@gmail.com>
2017-10-11 14:21:29 -0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,259 @@
+	jQuery(document).ready(function($){
+	//hide the subtle gradient layer (.pricing-list > li::after) when pricing table has been scrolled to the end (mobile version only)
+	checkScrolling($('.pricing-body'));
+	$(window).on('resize', function(){
+		window.requestAnimationFrame(function(){checkScrolling($('.pricing-body'))});
+	});
+	$('.pricing-body').on('scroll', function(){ 
+		var selected = $(this);
+		window.requestAnimationFrame(function(){checkScrolling(selected)});
+	});
+
+	function checkScrolling(tables){
+		tables.each(function(){
+			var table= $(this),
+				totalTableWidth = parseInt(table.children('.pricing-features').width()),
+		 		tableViewport = parseInt(table.width());
+			if( table.scrollLeft() >= totalTableWidth - tableViewport -1 ) {
+				table.parent('li').addClass('is-ended');
+			} else {
+				table.parent('li').removeClass('is-ended');
+			}
+		});
+	}
+
+	//switch from monthly to annual pricing tables
+	bouncy_filter($('.pricing-container'));
+
+	function bouncy_filter(container) {
+		container.each(function(){
+			var pricing_table = $(this);
+			var filter_list_container = pricing_table.children('.pricing-switcher'),
+				filter_radios = filter_list_container.find('input[type="radio"]'),
+				pricing_table_wrapper = pricing_table.find('.pricing-wrapper');
+
+			//store pricing table items
+			var table_elements = {};
+			filter_radios.each(function(){
+				var filter_type = $(this).val();
+				table_elements[filter_type] = pricing_table_wrapper.find('li[data-type="'+filter_type+'"]');
+			});
+
+			//detect input change event
+			filter_radios.on('change', function(event){
+				event.preventDefault();
+				//detect which radio input item was checked
+				var selected_filter = $(event.target).val();
+
+				//give higher z-index to the pricing table items selected by the radio input
+				show_selected_items(table_elements[selected_filter]);
+
+				//rotate each pricing-wrapper 
+				//at the end of the animation hide the not-selected pricing tables and rotate back the .pricing-wrapper
+				
+				if( !Modernizr.cssanimations ) {
+					hide_not_selected_items(table_elements, selected_filter);
+					pricing_table_wrapper.removeClass('is-switched');
+				} else {
+					pricing_table_wrapper.addClass('is-switched').eq(0).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {		
+						hide_not_selected_items(table_elements, selected_filter);
+						pricing_table_wrapper.removeClass('is-switched');
+						//change rotation direction if .pricing-list has the .bounce-invert class
+						if(pricing_table.find('.pricing-list').hasClass('bounce-invert')) pricing_table_wrapper.toggleClass('reverse-animation');
+					});
+				}
+			});
+		});
+	}
+	function show_selected_items(selected_elements) {
+		selected_elements.addClass('is-selected');
+	}
+
+	function hide_not_selected_items(table_containers, filter) {
+		$.each(table_containers, function(key, value){
+	  		if ( key != filter ) {	
+				$(this).removeClass('is-visible is-selected').addClass('is-hidden');
+
+			} else {
+				$(this).addClass('is-visible').removeClass('is-hidden is-selected');
+			}
+		});
+	}
+
+	var rangeSlider = function(){
+	  var slider = $('.range-slider'),
+	      range = $('.range-slider__range'),
+	      value = $('.range-slider__value');
+	    
+	  slider.each(function(){
+
+	    value.each(function(){
+	      var value = $(this).prev().attr('value');
+	      $(this).html(value+"%");
+	    });
+
+	    range.on('input', function(){
+	      $(this).next(value).html(this.value+"%");
+	    });
+	  });
+	};
+
+	rangeSlider();
+
+});
+
+
+  function ezBSAlert (options) {
+	var deferredObject = $.Deferred();
+	var defaults = {
+		type: "alert", //alert, prompt,confirm 
+		modalSize: 'modal-sm', //modal-sm, modal-lg
+		okButtonText: 'Ok',
+		cancelButtonText: 'Cancel',
+		yesButtonText: 'Yes',
+		noButtonText: 'No',
+		headerText: 'You are not truffle user',
+		messageText: 'Message',
+		alertType: 'default', //default, primary, success, info, warning, danger
+		inputFieldType: 'text', //could ask for number,email,etc
+	}
+	$.extend(defaults, options);
+  
+	var _show = function(){
+		var headClass = "navbar-default";
+		switch (defaults.alertType) {
+			case "primary":
+				headClass = "alert-primary";
+				break;
+			case "success":
+				headClass = "alert-success";
+				break;
+			case "info":
+				headClass = "alert-info";
+				break;
+			case "warning":
+				headClass = "alert-warning";
+				break;
+			case "danger":
+				headClass = "alert-danger";
+				break;
+        }
+		$('BODY').append(
+			'<div id="ezAlerts" class="modal fade">' +
+			'<div class="modal-dialog" class="' + defaults.modalSize + '">' +
+			'<div class="modal-content">' +
+			'<div id="ezAlerts-header" class="modal-header ' + headClass + '">' +
+			'<button id="close-button" type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>' +
+			'<h4 id="ezAlerts-title" class="modal-title">Modal title</h4>' +
+			'</div>' +
+			'<div id="ezAlerts-body" class="modal-body">' +
+			'<div id="ezAlerts-message" ></div>' +
+			'</div>' +
+			'<div id="ezAlerts-footer" class="modal-footer">' +
+			'</div>' +
+			'</div>' +
+			'</div>' +
+			'</div>'
+		);
+
+		$('.modal-header').css({
+			'padding': '15px 15px',
+			'-webkit-border-top-left-radius': '5px',
+			'-webkit-border-top-right-radius': '5px',
+			'-moz-border-radius-topleft': '5px',
+			'-moz-border-radius-topright': '5px',
+			'border-top-left-radius': '5px',
+			'border-top-right-radius': '5px'
+		});
+    
+		$('#ezAlerts-title').text(defaults.headerText);
+		$('#ezAlerts-message').html(defaults.messageText);
+
+		var keyb = "false", backd = "static";
+		var calbackParam = "";
+		switch (defaults.type) {
+			case 'alert':
+				keyb = "true";
+				backd = "true";
+				$('#ezAlerts-footer').html('<button class="btn btn-' + defaults.alertType + '">' + defaults.okButtonText + '</button>').on('click', ".btn", function () {
+					calbackParam = true;
+					$('#ezAlerts').modal('hide');
+				});
+				break;
+			case 'confirm':
+				var btnhtml = '<button id="ezok-btn" class="btn btn-primary">' + defaults.yesButtonText + '</button>';
+				if (defaults.noButtonText && defaults.noButtonText.length > 0) {
+					btnhtml += '<button id="ezclose-btn" class="btn btn-default">' + defaults.noButtonText + '</button>';
+				}
+				$('#ezAlerts-footer').html(btnhtml).on('click', 'button', function (e) {
+						if (e.target.id === 'ezok-btn') {
+							calbackParam = true;
+							$('#ezAlerts').modal('hide');
+						} else if (e.target.id === 'ezclose-btn') {
+							calbackParam = false;
+							$('#ezAlerts').modal('hide');
+						}
+					});
+				break;
+			case 'prompt':
+				$('#ezAlerts-message').html(defaults.messageText + '<br /><br /><div class="form-group"><input type="' + defaults.inputFieldType + '" class="form-control" id="prompt" /></div>');
+				$('#ezAlerts-footer').html('<button class="btn btn-primary">' + defaults.okButtonText + '</button>').on('click', ".btn", function () {
+					calbackParam = $('#prompt').val();
+					$('#ezAlerts').modal('hide');
+				});
+				break;
+		}
+   
+		$('#ezAlerts').modal({ 
+          show: false, 
+          backdrop: backd, 
+          keyboard: keyb 
+        }).on('hidden.bs.modal', function (e) {
+			$('#ezAlerts').remove();
+			deferredObject.resolve(calbackParam);
+		}).on('shown.bs.modal', function (e) {
+			if ($('#prompt').length > 0) {
+				$('#prompt').focus();
+			}
+		}).modal('show');
+	}
+    
+  _show();  
+  return deferredObject.promise();    
+}
+
+
+
+$(document).ready(function(){  $(".btnAlert").on("click", function(){  	
+  var prom = ezBSAlert({
+      messageText: "You should suscribe to one of our plans",
+      alertType: "danger"
+    }).done(function (e) {
+      $("body").append('<div>Callback from alert</div>');
+    });
+  });   
+
+  $("#btnConfirm").on("click", function(){  	
+    ezBSAlert({
+      type: "confirm",
+      messageText: "Do you want to chek our plans?",
+      alertType: "info"
+    }).done(function (e) {
+      $("body").append('<div>Callback from confirm ' + e + '</div>');
+    });
+  });  
+
+  $("#btnPrompt").on("click", function(){  	
+    ezBSAlert({
+      type: "prompt",
+      messageText: "Enter Something",
+      alertType: "primary"
+    }).done(function (e) {
+      ezBSAlert({
+        messageText: "You entered: " + e,
+        alertType: "success"
+      });
+    });   
+   }); 
+  
+});
